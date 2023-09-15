fbc65086edf0d0bf02c2100339229040d2cf4d10
Adam Bialas <BialasA@gmail.com>
2023-09-15 08:15:08 +0200

    Update author.rb
    fix for validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,7 +4,9 @@ class Author < ApplicationRecord
   has_many :books, dependent: :destroy
 
   validates :first_name, presence: true
+  validates_length_of :first_name, maximum: 32
   validates :last_name, presence: true
+  validates_length_of :last_name, maximum: 32
   validates :genre, presence: true
   validates_inclusion_of :genre, in: GENRE_LIST
 end
