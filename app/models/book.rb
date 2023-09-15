1876db35803be69180ae3541d8f3ab9f03fb5ed1
Adam Bialas <BialasA@gmail.com>
2023-09-15 08:17:13 +0200

    Update book.rb
    add validation for book title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author, foreign_key: "author_id", class_name: "Author"
+  validates :title, presence: true
+  validates_length_of :title, maximum: 128
 
   def gen_cover_url
     return if self.cover.nil?
